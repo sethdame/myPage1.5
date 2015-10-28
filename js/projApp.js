7f2379b26959ffe4c3aee7ffb2913c83e3a58042
sethdame <seth.e.dame@gmail.com>
2015-10-28 00:45:11 -0600

    acc tweak

--- js/projApp.js
+++ js/projApp.js
@@ -1,8 +1,8 @@
 $(document).ready(function() {
 	$('#projSlider').zAccordion({
 		width: 900,
-		speed: 1200,
-		timeout: 9000,
+		speed: 1500,
+		timeout: 10000,
 		slideClass: "slider",
 		animationStart: function() {
 			$('#projSlider').find('li.slide-previous div').fadeOut();
